12399eadebb8bb8c0d87d40eb9a1e69ad1f6653b
jeelpatel231 <jeelpatel231@gmail.com>
2023-07-25 15:57:55 +0530

    dont drop schema in production

--- src/lib/typeORM/Database.ts
+++ src/lib/typeORM/Database.ts
@@ -4,13 +4,13 @@ import { DataSource } from "typeorm";
 import { BaseDataSourceConfig } from "./BaseConfig";
 
 export const AppDataSource = new DataSource({
+  // TODO : move to env file
   type: "postgres",
   host: "localhost",
   port: 5432,
   username: "postgres",
   password: "password",
   database: "postgres",
-  dropSchema: true,
   ...BaseDataSourceConfig
 })
 
