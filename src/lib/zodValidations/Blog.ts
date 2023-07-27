c83b28a198b9f24fd764fa437f80c2d9e891d1e7
jeelpatel231 <jeelpatel231@gmail.com>
2023-07-27 11:03:03 +0530

    make validation stricter

--- src/lib/zodValidations/Blog.ts
+++ src/lib/zodValidations/Blog.ts
@@ -3,7 +3,8 @@ import { v4 as uuidv4 } from 'uuid';
 
 export const BlogValidationSchema = z.object({
   id: z.string().default(uuidv4),
-  title: z.string(),
-  description: z.string(),
-  content: z.string(),
+  title: z.string().trim().min(1),
+  description: z.string().trim().min(1),
+  content: z.string().trim().min(1),
+  tags: z.string(),
 })
