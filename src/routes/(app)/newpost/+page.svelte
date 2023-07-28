49492f5bd6292feb84eb582ab2993691a68cecc7
jeelpatel231 <jeelpatel231@gmail.com>
2023-07-28 12:04:19 +0530

    display errors on form response

--- src/routes/(app)/newpost/+page.svelte
+++ src/routes/(app)/newpost/+page.svelte
@@ -10,18 +10,17 @@
 <div class="display-large">New Blog Post</div>
 <form method="post" class="max-w-3xl">
   <M3Input type="text" label="Title" name="title" />
+  <span class="error-text label-small">{form?.title ?? ""}</span>
   <M3Input type="text" label="Description" name="description" />
+  <span class="error-text label-small">{form?.description ?? ""}</span>
   <M3Input
     type="text"
     label="Tags"
     name="tags"
     note="A tag must be of only 1 word, multiple tags are seperated by spaces."
   />
+  <span class="error-text label-small">{form?.tags ?? ""}</span>
   <M3TextArea label="Content" name="content" expandable={true} />
+  <span class="block error-text label-small mb-4">{form?.content ?? ""}</span>
   <Mat3Button type="submit">Post Blog</Mat3Button>
 </form>
-<!--  ADD TAGS CHIPS -->
-
-{#if form?.fieldErrors}
-  {JSON.stringify(form)}
-{/if}
