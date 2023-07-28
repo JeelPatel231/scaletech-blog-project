4cd46eefc2b198ba06ba1647d71efab84a5b686c
jeelpatel231 <jeelpatel231@gmail.com>
2023-07-28 17:12:59 +0530

    bugfix: dont prefetch logout route

--- src/lib/components/NavRailAnchor.svelte
+++ src/lib/components/NavRailAnchor.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
   export let icon: string;
   export let label: string;
-
+  export let reload: true | "" | "off" | null | undefined = "off";
   export let href: string;
 </script>
 
-<a {href} class="w-16 flex flex-col justify-center items-center">
+<a
+  {href}
+  data-sveltekit-reload={reload}
+  class="w-16 flex flex-col justify-center items-center"
+>
   <span
     class="text-center material-symbols-outlined on-primary-container-text rounded-full h-8 text-2xl mb-2 primary-container w-16"
   >
